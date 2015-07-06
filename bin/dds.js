fc3c2676f9a3723486eaa753bc38c28acf35b98a
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-06 08:48:10 +0200

    It's now possible to configure the keys and region via command line

--- bin/dds.js
+++ bin/dds.js
@@ -5,8 +5,9 @@
 /**
  * Register your service by using one of the following commands.
  * 
- * $ dds set mongo --host 10.9.128.3 --port 4444 --version 1.2
- * $ dds set mongo@1.2 10.9.128.3:4444
+ *     dds configure --accessKeyId :key --secretAccessKey :secret --region eu-west-1
+ *     dds set mongo --host 10.9.128.3 --port 4444 --version 1.2
+ *     dds set mongo@1.2 10.9.128.3:4444
  * 
  * The default port is 80 and the default version is 1.0
  * 
@@ -15,14 +16,20 @@
  */
 
 // module dependencies
-var minimist = require('minimist');
+var AWS = require('aws-sdk'),
+    fs = require('fs'),
+    path = require('path'),
+    os = require('os'),
+    minimist = require('minimist'),
+    pwuid = require('pwuid');
 
 // Parse the arguments
 var argv = minimist(process.argv.slice(2), {
-    string: ['host', 'version'],
+    string: ['host', 'version', 'accessKeyId', 'secretAccessKey', 'region'],
     default: {
         port: 80,
-        version: '1.0'
+        version: '1.0',
+        region: 'us-west-1'
     }
 });
 
@@ -34,7 +41,24 @@ function runTask(task) {
         throw new Error('Please provide a task.');
     }
     
-    if(task === 'set') {
+    if(task === 'configure') {
+        var credentials = [],
+            config = [];
+        
+        // Set the credentials
+        credentials.push('[default]');
+        credentials.push('aws_access_key_id=' + argv.accessKeyId);
+        credentials.push('aws_secret_access_key=' + argv.secretAccessKey);
+        
+        // Set the config
+        config.push('[default]');
+        config.push('region=' + argv.region);
+        config.push('output=json');
+        
+        fs.writeFileSync(path.join(pwuid().dir, '/.aws/credentials'), credentials.join(os.EOL));
+        fs.writeFileSync(path.join(pwuid().dir, '/.aws/config'), config.join(os.EOL));
+    }
+    else if(task === 'set') {
         var key = argv._[1],
             host = argv.host,
             port = argv.port,
